dfd72cf4bd8972c21e64e9d25aa933ffd575c1dc
Mustodik <dawbiemusdoiq30@gmail.com>
2022-04-06 14:53:01 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,12 @@
+const results=[
+{name:"Windy Dari", class:"Form one", marks:10},
+{name:"Dassah Felix", class:"Form one", marks:6},
+{name:"Ibrahim Mustodik", class:"Form one", marks:10},
+{name:"Assad Hashim", class:"Form one", marks:5},
+{name:"Suraj Suleman", class:"Form one", marks:9},
+{name:"Awudu Suraj", class:"Form one", marks:2},
+{name:"Musah Rakib ", class:"Form one", marks:1},
+{name:"Musah Felix", class:"Form one", marks:8}
+]
+
+module.exports=results
